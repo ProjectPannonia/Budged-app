afb3ba691400c012181346fa19a0afc92a1ce536
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-10-05 08:12:15 +0200

    Update app.js
    6 / 19 Deleting an item from our budget controller finished

--- app.js
+++ app.js
@@ -66,16 +66,21 @@ let budgetController = (function() {
             
         },
 
-        deleteBudget: function(type, id) {
+        deleteItem: function(type, id) {
             let ids, index;
             // id = 3
             //data.allItems[type][id]
-            
+            // ids = [1 2 4 6 8];
+            //
             ids = data.allItems[type].map(function(current) {
                 return current.id;
             });
             
             index = ids.indexOf(id);
+
+            if ( index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
         },
 
         calculateBudget: function() {
@@ -260,10 +265,10 @@ let controller = (function(budgetCtrl, UICtrl) {
             // inc-1
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             // 1. delete the item from the data structure
-
+            budgetCtrl.deleteItem(type, ID);
             // 2. Delete the item from the UI
 
             // 3. Update and show the new budget
