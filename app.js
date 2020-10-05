56879f96da659039506a742d77f24ceea39d3199
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-10-05 13:09:23 +0200

    Update app.js
    6 / 21 Updating the percentages controller

--- app.js
+++ app.js
@@ -239,6 +239,16 @@ let controller = (function(budgetCtrl, UICtrl) {
         UIController.displayBudget(budget);
     };
 
+    let updatePercentages = function() {
+
+        // 1. Calculate percentages
+
+        // 2. Read percentages from the budget controller
+
+        // 3. Update the UI with the new percentages
+        
+    };
+
     let ctrlAddItem = function() {
         let input, newItem;
 
@@ -258,6 +268,10 @@ let controller = (function(budgetCtrl, UICtrl) {
 
             // 5. Calculate and update budget
             updateBudget();
+
+            // 6. Calculate and update percentages
+            updatePercentages();
+
         }
 
     };
@@ -282,7 +296,10 @@ let controller = (function(budgetCtrl, UICtrl) {
 
             // 3. Update and show the new budget
             updateBudget();
-            
+
+            // 4. Calculate and update percentages
+            updatePercentages();
+
         }
     };
 
